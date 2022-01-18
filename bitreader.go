b9697cf6de9edb14cb2e0565ec0d869846d5f7e8
Spenser Black <spenserblack01@gmail.com>
2022-01-18 11:58:58 -0500

    Add `ReadBit`

--- bitreader.go
+++ bitreader.go
@@ -4,6 +4,10 @@ import "io"
 
 const byteSize int = 8
 
+// Bit is mainly for documentation purposes, and to help clarify the expected
+// return value.
+type Bit = byte
+
 // Reader reads the bits from a reader that reads bytes.
 type Reader struct {
 	// R is the underlying reader that reads bytes.
@@ -15,6 +19,19 @@ type Reader struct {
 	// Index is the index of the bit.
 	index  int
 }
+
+ // ReadBit reads a single bit.
+ func (r *Reader) ReadBit() (Bit, error) {
+	var err error
+	if r.index == 0 {
+		_, err = r.R.Read(r.Bytes)
+	}
+	byteIndex := r.index / byteSize
+	bitIndex := r.index % byteSize
+	b := r.Bytes[byteIndex] >> (7 - bitIndex)
+	b &= 1 // Normalize
+	return b, err
+ }
 
 // IncIndex increments the index, and loops it to 0 when the max index is
 // reached.
