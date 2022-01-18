2933a4e1d2ac64750f98144435530250e07d49ed
Spenser Black <spenserblack01@gmail.com>
2022-01-18 11:24:39 -0500

    Implement incrementing index

--- bitreader.go
+++ bitreader.go
@@ -1,7 +1,26 @@
-package main
+package bitreader
 
-import "fmt"
+import "io"
 
-func main() {
-	fmt.Println("Hello, World!")
+const byteSize int = 8
+
+// Reader reads the bits from a reader that reads bytes.
+type Reader struct {
+	// R is the underlying reader that reads bytes.
+	R      io.Reader
+	// Bytes stores the bytes that are read from the reader. When the last
+	// available bit is read, the next set of bytes will be read from R. The
+	// length of Bytes can be though of as the chunk size.
+	Bytes  []byte
+	// Index is the index of the bit.
+	index  int
+}
+
+// IncIndex increments the index, and loops it to 0 when the max index is
+// reached.
+func (r *Reader) incIndex() {
+	r.index++
+	if r.index >= len(r.Bytes) * byteSize {
+		r.index = 0
+	}
 }
