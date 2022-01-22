2c272ae60de175119f3a0ecb5b1218c27e902799
Spenser Black <spenserblack01@gmail.com>
2022-01-22 15:24:01 -0500

    Initial version
    Provides the `ReadBit` method.

--- bitreader.go
+++ bitreader.go
@@ -1,7 +1,79 @@
-package main
+package bitreader
 
-import "fmt"
+import "io"
 
-func main() {
-	fmt.Println("Hello, World!")
+const byteSize int = 8
+
+// Bit is mainly for documentation purposes, and to help clarify the expected
+// return value.
+type Bit = byte
+
+// Reader reads the bits from a reader that reads bytes.
+type Reader struct {
+	// R is the underlying reader that reads bytes.
+	r io.Reader
+	// Bytes stores the bytes that are read from the reader. When the last
+	// available bit is read, the next set of bytes will be read from R. The
+	// length of Bytes can be though of as the chunk size.
+	bytes []byte
+	// Index is the index of the bit.
+	index int
+	// Err is the error returned by the underlying reader that should be
+	// returned when all bits are read.
+	err error
+	// Avail is the number of available bytes that can be read.
+	avail int
+}
+
+// New creates a new bit reader. The amount of bytes to be read at a time is
+// set by chunkSize.
+func New(r io.Reader, chunkSize int) *Reader {
+	return &Reader{
+		r: r,
+		bytes: make([]byte, chunkSize),
+	}
+}
+
+ // ReadBit reads a single bit.
+ func (r *Reader) ReadBit() (Bit, error) {
+	var err error
+	if r.index == 0 {
+		r.avail, r.err = r.r.Read(r.bytes)
+	}
+
+	b := r.currentBit()
+	if r.avail == 0 || r.byteIndex() > r.avail {
+		err = r.err
+	} else {
+		r.incIndex()
+	}
+	return b, err
+ }
+
+// IncIndex increments the index, and loops it to 0 when the max index is
+// reached.
+func (r *Reader) incIndex() {
+	r.index++
+	r.index %= r.avail * byteSize
+}
+
+// ByteIndex gets the index of the byte that the bit is read from.
+func (r Reader) byteIndex() int {
+	return r.index / byteSize
+}
+
+// BitIndex gets the index of the bit within the byte (counting from the left).
+func (r Reader) bitIndex() int {
+	return r.index % byteSize
+}
+
+// CurrentByte gets the current byte to read.
+func (r Reader) currentByte() byte {
+	return r.bytes[r.byteIndex()]
+}
+
+// CurrentBit gets the current bit to read.
+func (r Reader) currentBit() Bit {
+	bitIndex := 7 - r.bitIndex()
+	return (r.currentByte() >> bitIndex) & 1
 }
---
+++ bitreader_test.go
@@ -0,0 +1,78 @@
+package bitreader
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+// TestIncIndex tests that the index will increment or wrap.
+func TestIncIndex(t *testing.T) {
+	tests := []struct{
+		bytes   int
+		index   int
+		want    int
+	}{
+		{1, 0, 1}, {1, 7, 0},
+		{2, 0, 1}, {2, 7, 8}, {2, 15, 0},
+	}
+
+	for _, tt := range tests {
+		r := &Reader{
+			bytes: make([]byte, tt.bytes),
+			avail: tt.bytes,
+			index: tt.index,
+		}
+		r.incIndex()
+		if r.index != tt.want {
+			t.Errorf(
+				`Increment from %d = %d, want %d`, 
+				tt.index, r.index, tt.want,
+			)
+		}
+	}
+}
+
+// TestNewBitReader tests that a new bit reader is properly created.
+func TestNewBitReader(t *testing.T) {
+	r := New(nil, 4)
+	if r.r != nil {
+		t.Fatalf(`r.r = %v, want nil`, r.r)
+	}
+	if l := len(r.bytes); l != 4 {
+		t.Fatalf(`len(r.bytes) = %v, want 4`, l)
+	}
+	if r.index != 0 {
+		t.Fatalf(`r.index = %v, want 0`, r.index)
+	}
+}
+
+// TestReadBit tests that a single bit can be read.
+func TestReadBit(t *testing.T) {
+	buff := bytes.NewBuffer([]byte{0xF0, 0xA5, 0x08})
+	tests := []struct {
+		want Bit
+	}{
+		{1}, {1}, {1}, {1}, // F
+		{0}, {0}, {0}, {0}, // 0
+		{1}, {0}, {1}, {0}, // A
+		{0}, {1}, {0}, {1}, // 5
+		{0}, {0}, {0}, {0}, // 0
+		{1}, {0}, {0}, {0}, // 8
+	}
+	r := New(buff, 2)
+
+	for i, tt := range tests {
+		t.Logf(`bit %d (index %d)`, i, r.index)
+		bit, err := r.ReadBit()
+		if bit != tt.want {
+			t.Errorf(`bit = %d, want %d`, bit, tt.want)
+		}
+		if err != nil {
+			t.Fatalf(`err = %v, want nil`, err)
+		}
+	}
+	if _, err := r.ReadBit(); err != io.EOF {
+		t.Fatalf(`err = %v, want io.EOF`, err)
+	}
+}
